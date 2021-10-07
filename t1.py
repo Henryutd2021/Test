5b589c9675c0a2dbf778191cec1d01f4d2a957a9
Henryutd2021 <89668896+Henryutd2021@users.noreply.github.com>
2021-10-07 11:43:18 -0500

    new fun in t1.py

--- t1.py
+++ t1.py
@@ -12,3 +12,8 @@ for j in l:
     sqr(j)
     w = []
 
+def person(name, age, **kw):
+    print('name:', name, 'age:', age, 'other:', kw)
+    
+person('Adam', 45, gender='M', job='Engineer')
+extra = {'city': 'Beijing', 'job': 'Engineer'}
